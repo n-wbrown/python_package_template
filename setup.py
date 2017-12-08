6c180d6f6f206ee707991a644a2ecf203676693f
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-12-08 15:15:31 -0800

    setup.py change names

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import versioneer
 from setuptools import (setup, find_packages)
 
 
-setup(name     = 'pytpy',
+setup(name     = 'python_package_template',
       version  = versioneer.get_version(),
       cmdclass = versioneer.get_cmdclass(),
-      author   = 'SLAC National Accelerator Laboratory',
+      author   = 'ADD AUTHOR',
 
       packages    = find_packages(),
-      description = 'Generate Epics DB records from TwinCAT .tpy files',
+      description = 'ADD DESCRIPTION',
       include_package_data = True,
 )
